af4c19d1a7998d50dbc58644a9b97c8806920ee9
Trammell hudson <hudson@trmm.net>
2020-10-04 13:04:11 +0200

    snake crash works

--- snake.js
+++ snake.js
@@ -4,7 +4,8 @@
 AFRAME.registerComponent('snake', {
 	schema: {
 		dim: {type:'number', default:10, },
-		speed: {type:'number', default:0.5, },
+		speed: {type:'number', default:500, },
+		latest: {type:'selector', default:null, },
 	},
 
 	init: function()
@@ -23,8 +24,15 @@ AFRAME.registerComponent('snake', {
 			}
 		}
 
-		this.pos = new THREE.Vector3(0,0,0);
+		if (!this.pos)
+			this.pos = new THREE.Vector3(0,0,0);
+
 		this.last_add = 0;
+		this.reinit = false;
+
+		// delete any children that we might still have
+		while(this.el.firstChild)
+			this.el.removeChild(this.el.firstChild);
 	},
 
 	tick: function(time, dt)
@@ -32,12 +40,15 @@ AFRAME.registerComponent('snake', {
 		// convert from ms to seconds
 		dt /= 1000;
 
-		if (time - this.last_add)
+		if (time - this.last_add > this.data.speed)
 		{
+			if (this.reinit)
+				this.init();
+
+			console.log("time=", time);
 			this.last_add = time;
 			this.add();
 		}
-
 	},
 
 	trial_add: function()
@@ -71,25 +82,51 @@ AFRAME.registerComponent('snake', {
 		||  this.used[nx][ny][nz])
 			return false;
 
-		// this location is free
+		// this location is free; add two cubes from
+		// the previous position to this one
+		console.log("new cube ", nx, ny, nz);
+		this.add_cube(0.5, (this.pos.x + nx) / 2, (this.pos.y + ny) / 2, (this.pos.z + nz) / 2);
+		this.add_cube(0.5, nx, ny, nz);
+
+		this.used[nx][ny][nz] = true;
+		this.pos.x = nx;
+		this.pos.y = ny;
+		this.pos.z = nz;
+		return true;
+	},
+
+	add_cube: function(size,nx,ny,nz)
+	{
 		let cube = document.createElement('a-box');
 		cube.setAttribute('position', {
 			x: nx,
 			y: ny,
 			z: nz,
 		});
+		cube.setAttribute('width', size);
+		cube.setAttribute('height', size);
+		cube.setAttribute('depth', size);
 		cube.setAttribute('material', {
 			src: '#snake-texture',
-			color: '#c00080',
+			//color: '#c00080',
+			transparent: true,
+		});
+		cube.setAttribute('animation', {
+			property: "material.opacity",
+			from: 0.0,
+			to: 1.0,
+			dur: this.data.speed * 2,
+			loop: 0,
 		});
 
-		console.log("new cube ", nx, ny, nz);
 		this.el.appendChild(cube);
-		this.used[nx][ny][nz] = cube;
-		this.pos.x = nx;
-		this.pos.y = ny;
-		this.pos.z = nz;
-		return true;
+
+		if (this.data.latest)
+			this.data.latest.setAttribute('position', {
+				x: nx,
+				y: ny,
+				z: nz,
+			});
 	},
 
 	add: function()
@@ -97,7 +134,25 @@ AFRAME.registerComponent('snake', {
 		for(let i=0 ; i < 20 ; i++)
 		{
 			if (this.trial_add())
-				break;
+				return true;
 		}
+
+		// all done; flag that we need to re-init
+		for(let child of this.el.children)
+		{
+			child.setAttribute('animation', {
+				property: 'material.opacity',
+				from: 1.0,
+				to: 0.0,
+				dur: this.data.speed * 10,
+				loop: 0,
+			});
+		}
+
+		// ensure that we won't reinit until the fade has happened
+		this.last_add += this.data.speed * 15;
+		this.reinit = true;
+
+		return false;
 	}
 });
