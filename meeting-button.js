72cd5a4ba0108b0ddae5b510f6d63d31473e9337
Trammell hudson <hudson@trmm.net>
2020-11-15 20:56:14 +0100

    meeting: reverse orientation

--- meeting-button.js
+++ meeting-button.js
@@ -33,8 +33,8 @@ AFRAME.registerComponent('meeting-button', {
 			console.log(detail.position, detail.rotation);
 
 			// try rotation first to allow matrix update in teleport
-			player.avatarPOV.object3D.rotation.y = detail.rotation;
 			player.teleportTo(detail.position);
+			player.avatarPOV.object3D.rotation.y = detail.rotation;
 		});
 
 		console.log("meeting button init!")
