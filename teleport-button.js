d0c9cc3d800141f5e914971ad451e07bc3ed58d5
Trammell hudson <hudson@trmm.net>
2020-11-23 13:02:05 +0100

    teleport-button: defer to later for selector lookup

--- teleport-button.js
+++ teleport-button.js
@@ -3,7 +3,7 @@
  */
 AFRAME.registerComponent('teleport-button', {
 	schema: {
-		target: { type:'selector', default:null, },
+		target: { type:'string', default:null, },
 		offset: { type:'vec3', default:{ x:0, y:0, z:0 } },
 	},
 
@@ -12,11 +12,21 @@ AFRAME.registerComponent('teleport-button', {
 		this.target = new THREE.Vector3();
 		this.onClick = this.onClick.bind(this);
 		this.el.object3D.addEventListener('interact', this.onClick);
+		console.log("teleport-button: target=", this.data.target);
 	},
 
 	onClick: function(evt)
 	{
-		const target = this.data.target || this.el;
+		let target = this.el;
+
+		if (this.data.target)
+		{
+			const nodes = document.querySelectorAll(this.data.target);
+			if (nodes.length)
+				target = nodes[Math.floor(Math.random() * nodes.length)];
+			else
+				console.log("teleport-button: target=", this.data.target, " not found");
+		}
 
 		target.object3D.getWorldPosition(this.target);
 		this.target.add(this.data.offset);
