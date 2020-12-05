024e5da155fb893d908ae0ff7de0e687232a4842
adelle-ex <adelle.lin@tempus-ex.com>
2020-12-05 15:11:13 -0500

    ceiling lights now in a static wave

--- ceiling-lights.js
+++ ceiling-lights.js
@@ -3,20 +3,22 @@
  */
 AFRAME.registerComponent('ceilinglights', {
 	schema: {
-        lights_row: {type:'number', default:30, }, // number of lights to draw row
-        lights_col: {type:'number', default:10, }, // number of lights to draw column 
+        lights_row: {type:'number', default:5, }, // number of lights to draw row
+        lights_col: {type:'number', default:2, }, // number of lights to draw column 
         width: {type:'number', default:.05, }, // width of each light
         height: {type:'number', default:.5, }, // height of each light
         depth: {type:'number', default:.05, }, // depth of each light
 		range: {type:'number', default:5, }, // m at which the person can trigger animation
-		speed: {type:'number', default:1000, }, // ms to open the door
+        speed: {type:'number', default:1000, }, // ms to open the door
+        theta: {type:'number', default:0, }
 	},
 
 	init: function()
 	{
         console.log("CREATING LIGHTS");
 		// initial position is closed
-		this.open = 0;
+        this.open = 0;
+        var counter = 0;
 
 		// create the blades
         for(let i = 0 ; i < this.data.lights_row ; i++){
@@ -32,7 +34,7 @@ AFRAME.registerComponent('ceilinglights', {
                 };
                 div.setAttribute('position', {
                     x: i/2,
-                    y: 0,
+                    y: - Math.sin(counter) / 2,
                     z: j/3,
                 });
                 // div.setAttribute('rotation', {
@@ -75,75 +77,39 @@ AFRAME.registerComponent('ceilinglights', {
                 div.appendChild(light);
                 this.el.appendChild(div);
             }
+            counter += i/this.data.lights_row;
         }
     },
 
 	tick: function(time, dt)
 	{
-		// convert from ms to seconds
-        dt /= 1000;
-        console.log("TIME")
-        console.log(dt)
+		// // convert from ms to seconds
+        // dt /= 1000;
+        // // console.log("TIME")
+        // // console.log(dt)
 
-		var pos = this.el.object3D.position;
-        var min_dist = 10000;
-
-        // start an animation to open the door
-        this.open = true;
-        // for(let light of this.el.children)
-        // {
-        this.el.setAttribute('position', {
-            x:-2, y: 3 - dt, z:-12.5 });
-
-/*
-		// find the closest interactable object
-		for(obj of document.querySelectorAll('.interactable'))
-		{
-			let dist = pos.distanceTo(obj.object3D.position);
-			if (min_dist > dist)
-				min_dist = dist;
-		}
-*/
-
-		// find the closest player
-		for(obj of document.querySelectorAll("a-entity[player-info]")) {
-            let dist = pos.distanceTo(obj.object3D.position);
-            // console.log("POS");
-            // console.log(dist);
-			if (min_dist > dist)
-				min_dist = dist;
-		}
-
-		// if distance is less than the range, open the door
-		// if (min_dist < this.data.range && !this.open)
-		// {
-		// 	// start an animation to open the door
-		// 	this.open = true;
-		// 	console.log("OPENING");
-		// 	// for(let light of this.el.children)
-		// 	// {
-        //     this.el.setAttribute('animation', {
-        //         property: 'position',
-        //         to: { x:0, y: 1, z:-6 },
-        //         dur: this.data.speed,
-        //         loop: 0,
-        //     });
-		// 	// }
-		// } else
-		// if (min_dist > this.data.range && this.open)
-		// {
-		// 	// start the animation to close the door
-		// 	this.open = true;
-        //     console.log("CLOSING");
-		// 	// for(let light of this.el.children)
-		// 	// {
-        //     this.el.setAttribute('animation', {
-        //         property: 'position',
-        //         to: { x:0, y: 3, z:-6 },
-        //         dur: this.data.speed,
-        //         loop: 0,
-        //     });
-		// 	// }
-		// }
+        // var counter = 0;
+        // theta = this.data.theta;
+        // theta += 0.1;
+        
+        // // console.log(theta);
+        // var lights = this.el.children;
+        // // console.log(lights.length)
+        // for (var i = 0; i < lights.length; i++) {
+        //     var i_u = i / this.data.lights_row; //row
+        //     var i_v = i % this.data.lights_row; // column
+        //     // counter = i_u / this.data.lights_row;
+        //     var light = lights[i];
+            
+        //     pos_x = light.getAttribute('position').x;
+        //     pos_y = light.getAttribute('position').y;
+        //     pos_z = light.getAttribute('position').z;
+        //     this.el.setAttribute('position', {
+        //         x: pos_x, 
+        //         y: Math.sin(i_u + theta) / 2 + 0.5,
+        //         z: pos_z});
+        // }
+        // this.data.theta = theta;
+	
 	},
 });
